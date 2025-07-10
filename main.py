e2c8fae7fd5eef29915a6e1756280b2e4e71845e
Shubkin1isp22 <163159716+Shubkin1isp22@users.noreply.github.com>
2025-07-10 21:48:48 +0700

    Version 0.1.1 - bugfix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import glob
 
-from telebot import TeleBot
+from telebot import TeleBot, apihelper
 import os, yt_dlp
 from yt_dlp import YoutubeDL
 from dotenv import load_dotenv
@@ -8,6 +8,8 @@ from dotenv import load_dotenv
 load_dotenv()
 token = os.getenv("TOKEN")
 
+apihelper.READ_TIMEOUT = 60
+apihelper.CONNECTION_TIMEOUT = 60
 bot = TeleBot(token)
 
 @bot.message_handler(commands=["start", "help"])
@@ -42,17 +44,28 @@ def download(message):
             'preferedformat': 'mp4' # конвертируем в формат mp4
         }]
     }
+    bot.send_message(chat_id, "Видео скачивается. Пожалуйста, подождите несколько минут.")
     with YoutubeDL(options) as ydl:
         ydl.download([url])
 
     all_video_of_user = glob.glob(f"downloads/{user_id}/*")
     last_video_of_user = max(all_video_of_user, key=os.path.getctime)
+
     with open(last_video_of_user, "rb") as video:
-        bot.send_document(chat_id, video)
+        file_size = os.path.getsize(video.name) / (1024 * 1024)  # в MB
+        print(f"Размер файла: {file_size:.2f} MB")
+        if file_size < 50:
+            try:
+                bot.send_document(chat_id, video, timeout=120)
+            except Exception as e:
+                print("Видео не отправилось пользвателю, ошибка типа: ", e)
+                bot.send_message(chat_id,"Скачать не удалось, попробуйте ещё раз")
+        else:
+            bot.send_message(chat_id, "Телеграмм не пропускает видео, весом больше 50mb для ботов")
 
 
 
 
 
 print("Бот в работе...")
-bot.polling()
+bot.polling(non_stop=True, timeout=100)
